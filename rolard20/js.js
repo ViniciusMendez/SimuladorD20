9a0a037fd32f00f552969e378887840307c60e8b
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-06-21 18:32:31 -0300

    o onclik nao funciona

---
+++ rolard20/js.js
@@ -0,0 +1,19 @@
+// sorteio dos dados d06 e d20
+var d20 = Math.floor(Math.random()*20 + 1);
+var d06 = Math.floor(Math.random()*6 + 1);
+
+console.log(d20)
+console.log(d06)
+
+// ____________________//______________________
+
+//testando se a destresa foi digitada e pedindo para colocala
+
+function contar() {
+    let num = document.getElementById('txtd')
+    let res = document.getElementById('res')
+    if (num == 0) {
+        window.alert('Por favor informe a sua destreza')
+    }
+    
+}
