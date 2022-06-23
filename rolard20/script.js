d571fbe831b0086f9e1ab2ba02bcfed8a0fef724
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-06-23 17:49:30 -0300

    Update script.js

--- rolard20/script.js
+++ rolard20/script.js
@@ -20,12 +20,37 @@ function calcular() {
         window.alert('Por favor informe a sua destreza')
 //caso aqeui ele coloca o valor digitado
     }else{
-        //res.innerHTML = ` Sua destreza é ${des}`
-        
+        imgdodo.setAttribute('src', 'd20/d20rodando.gif')
+        res.innerHTML = ` Sua destreza é ${des}`    
     }   
 }
 
-girar()
-function girar(){
-    res.innerHtml = 
-}
+setTimeout(function mostrar(){
+    switch (d20) {
+        case 1:
+            imgdodo.setAttribute('src', 'd20/d20lado01.png')
+            break
+        case 2:
+            imgdodo.setAttribute('src', 'd20/d20lado02.png')
+             break    
+         case 3:
+             imgdodo.setAttribute('src', 'd20/d20lado03.png')
+             break
+         case 4:
+             imgdodo.setAttribute('src', 'd20/d20lado04.png')
+             break
+        case 5:
+            imgdodo.setAttribute('src', 'd20/d20lado05.png')
+            break
+         case 6:
+            imgdodo.setAttribute('src', 'd20/d20lado06.png')
+            break
+         case 7:
+             imgdodo.setAttribute('src', 'd20/d20lado07.png')
+             break
+         case 8:
+             imgdodo.setAttribute('src', 'd20/d20lado08.png')
+              break
+
+    }
+}, 5000);
