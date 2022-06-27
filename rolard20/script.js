b2f746e8f28c8ed993a36c9d812b88a49dff8111
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-06-27 17:22:41 -0300

    upptade 4.5
    atualmente ele esta funcionado com o time de rodar o dado e exibindo o valor correspondente ao dado

--- rolard20/script.js
+++ rolard20/script.js
@@ -21,45 +21,78 @@ function calcular() {
 //caso aqeui ele coloca o valor digitado
     }else{
         imgdodo.setAttribute('src', 'd20/d20rodando.gif')
-        res.innerHTML = ` Sua destreza é ${des}`    
+        
+        //declarando cada caso e mostrando o lado de acorco com o numero sorteado
+        setTimeout(function mostrar(){
+            switch (d20) {
+                case 1:
+                    imgdodo.setAttribute('src', 'd20/d20lado01.png')
+                    break
+                case 2:
+                    imgdodo.setAttribute('src', 'd20/d20lado02.png')
+                     break    
+                case 3:
+                     imgdodo.setAttribute('src', 'd20/d20lado03.png')
+                     break
+                case 4:
+                     imgdodo.setAttribute('src', 'd20/d20lado04.png')
+                     break
+                case 5:
+                    imgdodo.setAttribute('src', 'd20/d20lado05.png')
+                    break
+                case 6:
+                    imgdodo.setAttribute('src', 'd20/d20lado06.png')
+                    break
+                case 7:
+                     imgdodo.setAttribute('src', 'd20/d20lado07.png')
+                     break
+                case 8:
+                     imgdodo.setAttribute('src', 'd20/d20lado08.png')
+                      break
+                case 9:
+                    imgdodo.setAttribute('src', 'd20/d20lado09.png')
+                     break  
+                case 10:
+                    imgdodo.setAttribute('src', 'd20/d20lado10.png')
+                     break
+                case 11:
+                    imgdodo.setAttribute('src', 'd20/d20lado11.png')
+                    break    
+                case 12:
+                    imgdodo.setAttribute('src', 'd20/d20lado12.png')
+                    break      
+                case 13:
+                    imgdodo.setAttribute('src', 'd20/d20lado13.png')
+                    break      
+                case 14:
+                    imgdodo.setAttribute('src', 'd20/d20lado14.png')
+                    break  
+                case 15:
+                    imgdodo.setAttribute('src', 'd20/d20lado15.png')
+                    break   
+                case 16:
+                    imgdodo.setAttribute('src', 'd20/d20lado16.png')
+                    break
+                case 17:
+                    imgdodo.setAttribute('src', 'd20/d20lado17.png')
+                    break  
+                case 18:
+                    imgdodo.setAttribute('src', 'd20/d20lado18.png')
+                    break 
+                case 19:
+                    imgdodo.setAttribute('src', 'd20/d20lado19.png')
+                    break       
+                case 20:
+                    imgdodo.setAttribute('src', 'd20/d20lado20.png')
+                    break  
+                default:
+                    break   
+                                        
+            }   
+            res.innerHTML = ` Sua destreza é ${des} e voce tirou ${d20}` 
+        }, 1000);   
+         
     }   
 }
 
-setTimeout(function mostrar(){
-    switch (d20) {
-        case 1:
-            imgdodo.setAttribute('src', 'd20/d20lado01.png')
-            break
-        case 2:
-            imgdodo.setAttribute('src', 'd20/d20lado02.png')
-             break    
-        case 3:
-             imgdodo.setAttribute('src', 'd20/d20lado03.png')
-             break
-        case 4:
-             imgdodo.setAttribute('src', 'd20/d20lado04.png')
-             break
-        case 5:
-            imgdodo.setAttribute('src', 'd20/d20lado05.png')
-            break
-        case 6:
-            imgdodo.setAttribute('src', 'd20/d20lado06.png')
-            break
-        case 7:
-             imgdodo.setAttribute('src', 'd20/d20lado07.png')
-             break
-        case 8:
-             imgdodo.setAttribute('src', 'd20/d20lado08.png')
-              break
-        case 9:
-            imgdodo.setAttribute('src', 'd20/d20lado09.png')
-             break  
-        case 10:
-            imgdodo.setAttribute('src', 'd20/d20lado10.png')
-             break
-        case 11:
-            imgdodo.setAttribute('src', 'd20/d20lado11.png')
-            break    
 
-    }
-}, 5000);
